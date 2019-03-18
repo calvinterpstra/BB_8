f7464d7c8116868b87f54f54e8a1286ea646fbf8
gerben2009 <46526648+gerben2009@users.noreply.github.com>
2019-03-18 10:59:43 +0100

    Update StateMachine.ino

--- StateMachine/StateMachine.ino
+++ StateMachine/StateMachine.ino
@@ -3,6 +3,17 @@
 #define BALANCE 'b'
 #define DRIVE 'd'
 
+//microphone variables
+int val = 0;
+int freq = 0;
+int counter = 0;
+unsigned long timer = 0;
+unsigned long start_time = 0;
+int STARTBAND_LOW = 1700;
+int STARTBAND_HIGH= 1900;  //band 1700-1900 Hz (group 111)
+
+
+
 unsigned long timeOffest;
 
 unsigned long getTime(){
@@ -30,6 +41,27 @@ void rest(){
 }
 
 void sound(){
+
+void setup(){
+  
+}
+
+void loop(){
+  timer = millis();
+  if((timer - start_time) > 100){
+    freq        = counter * 10;
+    start_time  = millis();
+    Serial.println(freq);
+    if ((freq>=STARTBAND_LOW)&&(freq<=STARTBAND_HIGH)}{
+      //switchState() 
+    }
+    counter     = 0;
+  }
+}
+
+void count() {
+  counter = counter + 1;
+}
 }
 
 void balance(){
@@ -40,6 +72,7 @@ void drive(){
 
 void setup() {
   Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(2), count , RISING); //Microphone pin
 }
 
 char state = REST;
