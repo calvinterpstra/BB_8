8e0d226f59da761ce74c23540d5fbdc7c857e5ca
kenricktrip1 <48315423+kenricktrip1@users.noreply.github.com>
2019-03-18 13:06:59 +0100

    receiver fix

--- Remote_control/Receiver/Receiver.ino
+++ Remote_control/Receiver/Receiver.ino
@@ -2,44 +2,23 @@
 
 SoftwareSerial HC12(10, 11); // HC-12 TX Pin, HC-12 RX Pin
 
-char incomingByte;
+#define setPin 6
+
+byte incomingByte;
 String readBuffer = "";
-int currentAngle = 0;
-
-
 
 void setup() {
   Serial.begin(9600);             // Open serial port to computer
   HC12.begin(9600);               // Open serial port to HC12
+  pinMode(setPin, OUTPUT);
+  digitalWrite(setPin, HIGH);     // HC-12 normal mode
 }
+
 void loop() {
-  readBuffer = "";
-  boolean start = false;
-  // Reads the incoming angle
+  // ==== Storing the incoming data into a String variable
   while (HC12.available()) {             // If HC-12 has data
-    incomingByte = HC12.read();          // Store each icoming byte from HC-12
-    delay(5);
-    // Reads the data between the start "s" and end marker "e"
-    if (start == true) {
-      if (incomingByte != 'e') {
-        readBuffer += char(incomingByte);    // Add each byte to ReadBuffer string variable
-      }
-      else {
-        start = false;
-      }
-    }
-    // Checks whether the received message statrs with the start marker "s"
-    else if ( incomingByte == 's') {
-      start = true; // If true start reading the message
-    }
-  }
-  // Converts the string into integer
-  currentAngle = readBuffer.toInt();
-  // Makes sure it uses angles between 0 and 180
-  if (currentAngle > 0 && currentAngle < 180) {
-    // Convert angle value to steps (depending on the selected step resolution)
-    // A cycle = 200 steps, 180deg = 100 steps ; Resolution: Sixteenth step x16
-    currentAngle = map(currentAngle, 0, 180, 0, 1600); 
-    Serial.println(currentAngle); // Prints the angle on the serial monitor
+    incomingByte = HC12.read();
+    readBuffer += char(incomingByte);    // Add each byte to ReadBuffer string variable 
+    Serial.println(readBuffer);
   }
 }
