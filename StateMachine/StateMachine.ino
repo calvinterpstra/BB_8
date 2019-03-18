f1780323295d5c3d4eb90dfc5a2ab6d63bd8b810
gerben2009 <46526648+gerben2009@users.noreply.github.com>
2019-03-18 11:06:53 +0100

    Update StateMachine.ino

--- StateMachine/StateMachine.ino
+++ StateMachine/StateMachine.ino
@@ -11,8 +11,7 @@ unsigned long timer = 0;
 unsigned long start_time = 0;
 int STARTBAND_LOW = 1700;
 int STARTBAND_HIGH= 1900;  //band 1700-1900 Hz (group 111)
-
-
+void count() {counter = counter + 1;}
 
 unsigned long timeOffest;
 
@@ -41,28 +40,19 @@ void rest(){
 }
 
 void sound(){
-
-void setup(){
-  
-}
-
-void loop(){
   timer = millis();
   if((timer - start_time) > 100){
     freq        = counter * 10;
     start_time  = millis();
     Serial.println(freq);
     if ((freq>=STARTBAND_LOW)&&(freq<=STARTBAND_HIGH)}{
-      //switchState() 
+      updateState(BALANCE);
     }
     counter     = 0;
-  }
 }
 
-void count() {
-  counter = counter + 1;
-}
-}
+
+
 
 void balance(){
 }
