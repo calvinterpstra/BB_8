11d7b467ed68091647ae35f0ad13a04d8d58fc6a
gerben2009 <gerben2009@gmail.com>
2019-03-21 16:05:47 +0100

    fakka
    efiji

--- StateMachine/StateMachine.ino
+++ StateMachine/StateMachine.ino
@@ -3,73 +3,101 @@
 #define BALANCE 'b'
 #define DRIVE 'd'
 #include "microphoneListener.h"
- 
+#define microphone_pin 2
+
+///SCRIPT FOR NANO
 
 unsigned long timeOffest;
 
-unsigned long getTime(){
+unsigned long getTime()
+{
     return millis() - timeOffest;
 }
 
-void resetTimer(){
+void resetTimer()
+{
     timeOffest = millis();
 }
 
-
-
-void rest(){
+void rest()
+{
 }
 
-
-void balance(){
+void balance()
+{
+    digitalWrite(3,HIGH);
+    digitalWrite(5,HIGH);
+    digitalWrite(7,HIGH);
+  
 }
 
-void drive(){
+void drive()
+{
+    digitalWrite(3,LOW);
+    digitalWrite(5,LOW);
+    digitalWrite(7,LOW);
 }
 
-void setup() {
-  Serial.begin(9600);
-  setupMicrophone();
+void setup()
+{
+    Serial.begin(9600);
+    setupMicrophone();
+    pinMode(3, OUTPUT);
+    pinMode(5, OUTPUT);
+    pinMode(7, OUTPUT);
+
 }
 
 char state = REST;
-void loop() {
-    switch (state) {
-        case REST:
-            rest();
-            break;
-        case SOUND:
-            byte signal_received = listenMicrophone();
-            if (signal_received){ updateState(BALANCE);}
-            break;
-        case BALANCE:
-            balance();
-            break;
-        case DRIVE:
-            drive();
-            break;
-       knmbkekvnb default:
-            break;
+void updateState(char nextState)
+{
+    state = nextState;
+    Serial.print("new state:");
+    Serial.println(state);
+}
+
+void loop()
+{
+    switch (state)
+    {
+    case REST:
+        rest();
+        break;
+    case SOUND:
+        //byte signal_received = ;
+        if (listenMicrophone())
+        {
+            updateState(BALANCE);
+            detachInterrupt(digitalPinToInterrupt(microphone_pin)); //done listening
+        }
+        break;
+    case BALANCE:
+        balance();
+        break;
+    case DRIVE:
+        drive();
+        break;
+    case default:
+        break;
     }
     //Serial.write(state);
     readKeyboard();
 }
 
-void readKeyboard(){
+void readKeyboard()
+{
     int incomingByte = 0;
-    if (Serial.available() > 0) {
+    if (Serial.available() > 0)
+    {
         int incomingByte = Serial.read();
-        if (incomingByte==63){
-          Serial.print("Current state:");
-          Serial.println(state);
+        if (incomingByte == 63)
+        {
+            Serial.print("Current state:");
+            Serial.println(state);
         }
-        else if (incomingByte != 10){    updateState(incomingByte); }
+        else if (incomingByte != 10)
+        {
+            updateState(incomingByte);
+        }
     }
-}
-
-
-void updateState(char nextState){
-    state = nextState;
-    Serial.print("new state:");
-    Serial.println(state);
 }
--- StateMachine/microphoneListener.h
+++ StateMachine/microphoneListener.h
@@ -7,12 +7,12 @@ unsigned long start_time = 0;
 int STARTBAND_LOW = 1700;
 int STARTBAND_HIGH= 1900;  //band 1700-1900 Hz (group 111)
 void count() {counter = counter + 1;}
-
+#define microphone_pin 2
 
 
 
 void setupMicrophone(){
-      attachInterrupt(digitalPinToInterrupt(2), count , RISING); //Microphone pin
+      attachInterrupt(digitalPinToInterrupt(microphone_pin), count , RISING); //Microphone pin
 }
 
 
