efbc5296e466d2af083afa3b9f8d9b6a04e7d9ea
Quinten <thequintendoornbos@gmail.com>
2019-03-25 13:26:23 +0100

    Gerben je code werkte nie

--- Remote_control_NANO/microphoneListener.h
+++ Remote_control_NANO/microphoneListener.h
@@ -4,31 +4,28 @@ int freq = 0;
 int counter = 0;
 unsigned long timer = 0;
 unsigned long start_time = 0;
-int STARTBAND_LOW = 1700;
-int STARTBAND_HIGH= 1900;  //band 1700-1900 Hz (group 111)
+#define F_MAX 1900 //Max. frequency [Hz]
+#define F_MIN 1700 //Min. frequency [Hz]
+#define MICROPHONE_PIN 2 //Pin connected to Microphone (MUST BE INTERRUPT PIN!)
+
+
 void count() {counter = counter + 1;}
-#define microphone_pin 13
-
-
 
 void setupMicrophone(){
-      attachInterrupt(digitalPinToInterrupt(microphone_pin), count , RISING); //Microphone pin
+      attachInterrupt(digitalPinToInterrupt(MICROPHONE_PIN), count , RISING); //Microphone pin
 }
 
 
 byte listenMicrophone(){
-    timer = millis();
-    if((timer - start_time) > 100){
-        freq        = counter * 10;
-        start_time  = millis();
-        Serial.println(freq);
-        if ((freq>=STARTBAND_LOW)&&(freq<=STARTBAND_HIGH)){
-          detachInterrupt(digitalPinToInterrupt(microphone_pin)); //done listening
-          return 1;
-          
-        }
-        counter     = 0;
-        return 0;
-}
-return 0;
+  timer = millis();
+  if((timer - start_time) > 100){
+    freq        = counter * 10;
+    start_time  = millis();
+    Serial.println(freq);
+    counter     = 0;
+    if(freq>=F_MIN && freq<=F_MAX){
+      return 1;
+    }
+  }
+  return 0;
 }
